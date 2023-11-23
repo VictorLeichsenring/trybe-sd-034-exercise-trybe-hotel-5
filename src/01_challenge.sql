d576a6695c4d10124d14401aa04fe76f1acd14b9
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-11-23 15:21:27 -0300

    Criada tabela amenities

---
+++ src/01_challenge.sql
@@ -0,0 +1,21 @@
+CREATE TABLE amenities (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  single_bed INTEGER NOT NULL,
+  double_bed INTEGER NOT NULL,
+  breakfast BOOLEAN NOT NULL,
+  minibar BOOLEAN NOT NULL
+);
+
+INSERT INTO amenities(single_bed, double_bed, breakfast, minibar) VALUES
+(1,0,0,0),
+(1,0,1,0),
+(1,0,1,1),
+(2,0,0,0),
+(2,0,1,0),
+(2,0,1,1),
+(0,1,0,0),
+(0,1,1,0),
+(0,1,1,1),
+(1,1,0,0),
+(1,1,1,0),
+(1,1,1,1);
